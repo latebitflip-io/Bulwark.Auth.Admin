46e0ed3bcc0892856c917a97afbbb7b3b8c9487c
Fritz (Fredrick Seitz) <fredrick.seitz@gmail.com>
2023-09-07 15:55:28 -0400

    fix: make overloaded methods adjacent

--- src/Bulwark.Auth.Admin.Repositories/IRoleRepository.cs
+++ src/Bulwark.Auth.Admin.Repositories/IRoleRepository.cs
@@ -5,8 +5,8 @@ public interface IRoleRepository
 	Task Create(string role);
 	Task Delete(string role);
 	Task<RoleModel> Read(string name);
-	Task<List<string>> ReadByAccount(string accountId);
 	Task<List<RoleModel>> Read();
+	Task<List<string>> ReadByAccount(string accountId);
 	Task Update(string id, string name);
 	Task AddToAccount(string roleId, string accountId);
 	Task DeleteFromAccount(string role, string accountId);
